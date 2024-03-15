422c30ef5291d3ccb339f20c93c84e4c30ad7b7c
jhoan sebastian rodriguez urbina <jhoansebastianrodriguezurbina@jhoans-MacBook-Air.local>
2024-03-15 06:51:24 -0400

    adding the web server project

--- hello.py
+++
@@ -1,1 +0,0 @@
-print('hola care nalga')
---
+++ web-server/store.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    categories = r.json()
+    names = list(map(lambda category: category['name'],categories))
+
+    print(names)
