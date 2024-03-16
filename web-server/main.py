c6e111c547abd285968d0839e89dc57f732d4fb8
jhoan sebastian rodriguez urbina <jhoansebastianrodriguezurbina@jhoans-MacBook-Air.local>
2024-03-16 19:56:30 -0400

    agregando HTMLResponse

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,26 @@
 import store
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+app = FastAPI()
 
+#se agrega un recuzo que se llama decorador 
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/Contact', response_class=HTMLResponse)
+def get_list():
+    return """
+    <html>
+        <head>
+            <title>First time </title>
+        </head>
+        <body>
+            <h1> First time using FastAPI and web server</h1>
+            <p> this is just a test </p>
+        </body>
+    </html>
+    """
 def run():
     store.get_categories()
 
